bd8b0b47ba36bfccf0521d16dafeda958120bb80
Lorraine <48839557+djmissmills@users.noreply.github.com>
2021-06-21 19:43:39 +0100

    lazy load

--- .eleventy.js
+++ .eleventy.js
@@ -5,8 +5,6 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/images");
     eleventyConfig.addPassthroughCopy("src/fonts");
 
-
-
     return {
         dir: {
             input: "src",
@@ -14,6 +12,25 @@ module.exports = function(eleventyConfig) {
         },
     };
 
-
-
 };
+
+function lazyImages (eleventyConfig, userOptions = {}) {
+    const {parse} = require('node-html-parser')
+  
+    const options = {
+      name: 'lazy-images',
+      ...userOptions
+    }
+  
+    eleventyConfig.addTransform(options.extensions, (content, outputPath) => {
+      if (outputPath.endsWith('.html')) {
+        const root = parse(content);
+        const images = root.querySelectorAll('img');
+        images.forEach((img) => {
+          img.setAttribute('loading', 'lazy')
+        })
+        return root.toString()
+      }
+      return content;
+    })
+  }
