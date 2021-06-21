06e25bf8a79fb80ebb1bde5c1f583e93ba7df54c
Lorraine <48839557+djmissmills@users.noreply.github.com>
2021-06-21 16:02:16 +0100

    minify html

--- .eleventy.js
+++ .eleventy.js
@@ -14,6 +14,23 @@ module.exports = function(eleventyConfig) {
         },
     };
 
+};
 
 
+const htmlmin = require("html-minifier");
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+    // Eleventy 1.0+: use this.inputPath and this.outputPath instead
+    if( outputPath && outputPath.endsWith(".html") ) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true
+      });
+      return minified;
+    }
+
+    return content;
+  });
 };
