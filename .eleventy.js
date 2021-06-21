1f45288531d6345d9414947ac0145b8bf25a2460
Lorraine <48839557+djmissmills@users.noreply.github.com>
2021-06-21 15:57:59 +0100

    revert

--- .eleventy.js
+++ .eleventy.js
@@ -13,23 +13,7 @@ module.exports = function(eleventyConfig) {
             output: "public"
         },
     };
-};
 
 
-const htmlmin = require("html-minifier");
 
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
-    // Eleventy 1.0+: use this.inputPath and this.outputPath instead
-    if( outputPath && outputPath.endsWith(".html") ) {
-      let minified = htmlmin.minify(content, {
-        useShortDoctype: true,
-        removeComments: true,
-        collapseWhitespace: true
-      });
-      return minified;
-    }
-
-    return content;
-  });
 };
